2fa2e0d174380baee9ff9400383d5737a79d4b43
lujanfernaud <lujanfernaud@gmail.com>
2017-12-11 16:29:14 +0000

    Refactor signup_spec to use spies

--- spec/signup_spec.rb
+++ spec/signup_spec.rb
@@ -4,22 +4,31 @@ require "signup"
 describe Signup do
   describe "#save" do
     it "creates an account with one user" do
-      signup = Signup.new(email: "user@example.com", account_name: "Example")
+      email   = "user@example.com"
+      account = double("account", name: "Example")
 
+      stub_account_creation_with(account)
+      stub_user_creation_with(account, email)
+
+      signup = Signup.new(email: email, account_name: account.name)
       result = signup.save
 
-      expect(Account.count).to eq(1)
-      expect(Account.last.name).to eq("Example")
-      expect(User.count).to eq(1)
-      expect(User.last.email).to eq("user@example.com")
-      expect(User.last.account).to eq(Account.last)
+      expect(Account).to have_received(:create!).with(name: account.name)
+      expect(User).to have_received(:create!).with(account: account, email: email)
       expect(result).to be(true)
     end
   end
 
   describe "#user" do
     it "returns the user created by #save" do
-      signup = Signup.new(email: "user@example.com", account_name: "Example")
+      email   = "user@example.com"
+      account = double("account", name: "Example")
+      user    = double("user", account: account, email: email)
+
+      stub_account_creation_with(account)
+      stub_user_creation_with(account, email).and_return(user)
+
+      signup = Signup.new(email: email, account_name: account.name)
       signup.save
 
       result = signup.user
@@ -27,5 +36,16 @@ describe Signup do
       expect(result.email).to eq("user@example.com")
       expect(result.account.name).to eq("Example")
     end
+  end
+
+  def stub_account_creation_with(account)
+    allow(Account).to receive(:create!)
+      .with(name: account.name)
+      .and_return(account)
+  end
+
+  def stub_user_creation_with(account, email)
+    allow(User).to receive(:create!)
+      .with(account: account, email: email)
   end
 end
